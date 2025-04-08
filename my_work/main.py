664662f566e1acb5856e34594709cd1615532476
sphisi <143594972+sphisi@users.noreply.github.com>
2025-04-08 05:09:19 +0000

    toasty

--- my_work/main.py
+++ my_work/main.py
@@ -1,4 +1,5 @@
 #menu
+loginsexit = ["Ancient enimes r us", "I'm Your Father", "May the Force be with you", "patu", "Yoda", "I Am All The Jedi", "May the Force be with you"]
 logins = []
 menu_options = input("login: 1\nregister: 2\nexit: 3\nChoice: ")
 #if for 1
@@ -8,7 +9,7 @@ if menu_options == "1":
     with open("logins.csv") as file:
         print(login_username)
         for line in file:
-            if login_username in file:
+            if login_username in line:
                 listusername = []
                 listpassword = []
                 login_password = input("Whats your password: ")
@@ -17,19 +18,26 @@ if menu_options == "1":
                 # print(f"{username},{password}")
                 #get the index
                 index = login_password[1]
-                for line in file:
-                    if password in line:
+                #checks if password is in the value corrosponding the the username, dosent work :( properly
+                for line in index:
+                    if password in loginsexit:
+                        #asks if to change password or sxt
                         login_options = input("change password: 1\nLogout: 2\nChoice: ")
+                        #if change password opens files as read
                         if login_options == "1":
                             with open("logins.csv") as file:
+                                #creates new line to append file execpt for changeding line
                                 for line in file:
-                                    tempfile = []
-                                    
+                                    templist = []
+                                new_password = input("New password: ")
+                                
+                                #opens files as write and writes templist to file
+                            with open("logins", "w") as file:
+                                file.write(templist)
                         else:
                             exit
                     else:
                         print("invalid password")
-                        print(password)
             else:
                 print("username not found")
 #if for register 2
@@ -42,7 +50,7 @@ elif menu_options == "2":
         logins.append(f"{register_username},{register_password}")
     #add usernames to file
     with open("logins.csv", "a") as file:
-        # logins.append(f"{register_username},{register_password}")
+        loginsexit.append(f"{register_username},{register_password}")
         file.write(f"\n{register_username},{register_password}")
 #if for exit
 elif menu_options == "3":
